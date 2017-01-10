990848dafb3b72d7539aaa4533b7cdf97b7bacdd
sam9116 <sam.b5687@gmail.com>
2017-01-10 09:04:10 -0700

    cmput404 lab1

--- lab1.py
+++ lab1.py
@@ -2,7 +2,7 @@ import requests
 
 print requests.__version__
 
-response = requests.get("https://google.com/")
+response = requests.get("https://raw.githubusercontent.com/sam9116/cmput404-lab1/master/lab1.py")
 
 print response.status_code
 print response.text
